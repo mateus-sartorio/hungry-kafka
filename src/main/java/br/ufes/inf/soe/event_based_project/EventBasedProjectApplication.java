23694fd88b704da0a0e8675ec65dca5cf5726436
mateus-sartorio <mateussartorio39@gmail.com>
2026-03-28 00:09:45 -0300

    chore: formatting changes

--- src/main/java/br/ufes/inf/soe/event_based_project/EventBasedProjectApplication.java
+++ src/main/java/br/ufes/inf/soe/event_based_project/EventBasedProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventBasedProjectApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(EventBasedProjectApplication.class, args);
 	}
 
