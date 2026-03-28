978a0de033660ca185db8475073ee937f9f8ec17
mateus-sartorio <mateussartorio39@gmail.com>
2026-03-27 23:48:16 -0300

    feat: fist commit

---
+++ src/main/java/br/ufes/inf/soe/event_based_project/EventBasedProjectApplication.java
@@ -0,0 +1,13 @@
+package br.ufes.inf.soe.event_based_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBasedProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBasedProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufes/inf/soe/event_based_project/EventBasedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufes.inf.soe.event_based_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBasedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
