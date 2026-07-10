0d52dc95c4613b2dcf7b14e4700b40020733978c
mateus-sartorio <mateussartorio39@gmail.com>
2026-07-09 21:48:11 -0300

    refactor: update CartEvent

--- src/main/java/br/ufes/inf/soe/hungry_kafka/dto/CartEvent.java
+++ src/main/java/br/ufes/inf/soe/hungry_kafka/dto/CartEvent.java
@@ -1,12 +1,8 @@
 package br.ufes.inf.soe.hungry_kafka.dto;
 
-import java.time.Instant;
-
 public record CartEvent(
-        String eventId,
         CartAction action,
         Integer currentAmount,
         Integer productId,
-        Integer clientId,
-        Instant occurredAt) {
+        Integer clientId) {
 }
